2162c2c585e50210b1390e1f7cffaf0f5b0f7987
matthp9 <matt32375@gmail.com>
2024-07-16 12:06:15 -0700

    Use prettier

--- user.js
+++ user.js
@@ -1,10 +1,9 @@
 exports.postResponses = async (event) => {
-    const body = JSON.parse(event.body);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Post responses " + body?.test,
-      }),
-    };
+  const body = JSON.parse(event.body);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Post responses " + body?.test,
+    }),
   };
-  
+};
