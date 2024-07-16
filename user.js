98d8abd244a7a60cc79c6ebf8d742e1825ea96d5
matthp9 <matt32375@gmail.com>
2024-07-16 12:02:10 -0700

    Allow post to bring in parameters and test using Postman

--- user.js
+++ user.js
@@ -1,8 +1,9 @@
 exports.postResponses = async (event) => {
+    const body = JSON.parse(event.body);
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Post responses",
+        message: "Post responses " + body?.test,
       }),
     };
   };
